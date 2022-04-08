62f68a4cb93f558a05b8b36df7b7c57e363e66d8
Victor Velasques Angelino Perez <victorvelasques7@gmail.com>
2022-04-07 22:13:09 -0300

    Inserindo os projetos de database e context

---
+++ BancoDeDados/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Mapper/user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace User
+{
+    public class UserClient
+    {
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "O nome {0} é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O nome {0} pode ter no máximo {1} caractéres!")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "A Idade {0} é obrigatório!")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento {0} é obrigatório!")]
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
